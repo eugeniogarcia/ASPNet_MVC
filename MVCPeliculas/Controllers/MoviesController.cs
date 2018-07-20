735b9f41eff2d45f1d4b7e597de1144149ca5115
Eugenio <egsmartin@hotmail.com>
2018-07-20 14:31:29 +0200

    1st review

--- MVCPeliculas/Controllers/MoviesController.cs
+++ MVCPeliculas/Controllers/MoviesController.cs
@@ -14,12 +14,6 @@ namespace MVCPeliculas.Controllers
     {
         private MovieDBContext db = new MovieDBContext();
 
-        // GET: Movies
-        public ActionResult Index()
-        {
-            return View(db.Movies.ToList());
-        }
-
         // GET: Movies/Details/5
         public ActionResult Details(int? id)
         {
@@ -113,6 +107,34 @@ namespace MVCPeliculas.Controllers
             db.Movies.Remove(movie);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        public ActionResult Index(string movieGenre, string searchString)
+        {
+            var GenreLst = new List<string>();
+
+            var GenreQry = from d in db.Movies
+                           orderby d.Genre
+                           select d.Genre;
+
+            GenreLst.AddRange(GenreQry.Distinct());
+
+            ViewBag.movieGenre = new SelectList(GenreLst);
+
+            var movies = from m in db.Movies
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            return View(movies);
         }
 
         protected override void Dispose(bool disposing)
